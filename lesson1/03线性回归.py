20f0b6cce5c48acbc9a2e09f9e85dcaffb6c8ac2
Fangxin Wang <2681086480@qq.com>
2020-08-06 10:02:11 +0800

    线性回归模型

--- lesson1/03线性回归.py
+++ lesson1/03线性回归.py
@@ -6,15 +6,16 @@ torch.manual_seed(10)  # 随机数种子
 lr = 0.1  # 学习率
 
 # 创建训练数据
-x = torch.rand(20, 1) * 10  # x shape(20,1)
-y = 2 * x + (5 + torch.rand(20, 1))  # y
+x = torch.rand(20, 1) * 10  # x data, shape(20,1)
+y = 2 * x + (5 + torch.randn(20, 1))  # y data , shape(20,1)
 
 # 构造线性回归参数
 # requires_grad表示需要计算梯度
 w = torch.randn((1), requires_grad=True)
-b = torch.ones((1), requires_grad=True)
+b = torch.zeros((1), requires_grad=True)
 
 for iteration in range(1000):
+
     # 前向传播
     wx = torch.mul(w, x)
     y_pred = torch.add(wx, b)
@@ -32,8 +33,14 @@ for iteration in range(1000):
     # 绘图
     if iteration % 20 == 0:
         plt.scatter(x.data.numpy(), y.data.numpy())
-        plt.plot(x.data.numpy(), y_pred.data.numpy())
+        plt.plot(x.data.numpy(), y_pred.data.numpy(),'r-',lw=5)
+        # plt.text(2, 20, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
+        # 最小值和最大值
+        plt.xlim(1.5, 10)
+        plt.ylim(8, 28)
+        plt.title("Iteration: {}\nw: {} b: {}".format(iteration, w.data.numpy(), b.data.numpy()))
         plt.pause(0.5)
 
         if loss.data.numpy() < 1:
+            plt.pause(20)
             break
